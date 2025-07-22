96541785c5313c4bc581e31e73c60a9fc272bc72
Ananya-Yadav-Dev <anniyadav2807@gmail.com>
2025-07-23 02:38:11 +0530

    phase2-partd

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");//pbkdf2 hashing algorithm
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listings.js");
-const reviews = require("./routes/reviews.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 // const listEndpoints = require("express-list-endpoints");
 
@@ -51,17 +56,37 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
+app.get("/demouser",async (req,res)=>{
+  let fakeUser = new User({
+    username: "demouser",
+    email:"DEMO@GMAIL.COM"
+  });
+
+  let newUser = await User.register(fakeUser,"Helloworld");
+  res.send(newUser);
+})
+
 //Listings
-app.use("/listings",listings);
+app.use("/listings",listingRouter);
 
 //Reviews
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings/:id/reviews",reviewRouter);
+
+//Users
+app.use("/",userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
